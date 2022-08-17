daf1f2c816bac74439b1b53e7972b6485343fadf
joacochayle1 <joacochayle@gmail.com>
2022-08-17 12:08:03 -0300

    Subo mi programa de programacion

--- Juego_Ahorcado.py
+++ Juego_Ahorcado.py
@@ -1,13 +1,26 @@
 from random import choice
 
-palabras = ['panadero', 'dinosaurio', 'helipuerto', 'tiburon']
+palabras = ['panadero', 'dinosaurio', 'helipuerto', 'tiburon','cars','meteoro']
 letras_correctas = []
 letras_incorrectas = []
-intentos = 6
+intentos = 10
 aciertos = 0
 juego_terminado = False
 
-
+def pistas_palabras(palabra_elegida):
+    if palabra_elegida=="panadero":
+        print('La palabra que estas buscando tiene que ver con alguien que cocina cosas para que desayunes')
+    if palabra_elegida=="dinosaurio":
+        print("Estamos hablando de un tipo de 'animal' que vivia hace muchos años en la tierra")
+    if palabra_elegida=="helipuerto":
+        print("Lugar donde se estacionan vehiculos aéreos con aspas ")
+    if palabra_elegida=="tiburon":
+        print("Animal marino muy temido")
+    if palabra_elegida=="cars":
+        print("Película de Disney relacionada a los autos ")
+    if palabra_elegida=="meteoro":
+        print("película de carreras con autos con propulsores, el nombre tiene que ver con algo que mato a los dinosaurios")
+    return palabra_elegida
 def elegir_palbra(lista_palabras):
     palabra_elegida = choice(lista_palabras)
     letras_unicas = len(set(palabra_elegida))
@@ -41,3 +54,50 @@ def mostrar_nuevo_tablero(palabra_elegida):
             lista_oculta.append('-')
 
     print(' '.join(lista_oculta))
+
+def chequear_letra(letra_elegida, palabra_oculta, vidas, coincidencias):
+
+    fin = False
+
+    if letra_elegida in palabra_oculta:
+        letras_correctas.append(letra_elegida)
+        coincidencias += 1
+    else:
+        letras_incorrectas.append(letra_elegida)
+        vidas -= 1
+
+    if vidas == 0:
+        fin = perder()
+    elif coincidencias == letras_unicas:
+        fin = ganar(palabra_oculta)
+
+    return vidas, fin, coincidencias
+
+def perder():
+    print("Te has quedado sin vidas")
+    print("La palabra oculta era " + palabra)
+
+    return True
+
+
+def ganar(palabra_descubierta):
+    mostrar_nuevo_tablero(palabra_descubierta)
+    print("Felicitaciones, has encontrado la palabra!!!")
+
+    return True
+
+palabra, letras_unicas = elegir_palbra(palabras)
+
+while not juego_terminado:
+    print('\n' + '*' * 20 + '\n')
+
+    mostrar_nuevo_tablero(palabra)
+    print('\n')
+    print('Letras incorrectas: ' + '-'.join(letras_incorrectas))
+    print(f'Vidas: {intentos}')
+    print('\n' + '*' * 20 + '\n')
+    letra = pedir_letra()
+
+    intentos, terminado, aciertos = chequear_letra(letra,palabra,intentos,aciertos)
+
+    juego_terminado = terminado
