54d6d45113e4b78603001b38cfade09bac8e9ba2
joacochayle1 <joacochayle@gmail.com>
2022-08-30 21:35:32 -0300

    Subo mi programa de programacion

--- Juego_Ahorcado.py
+++ Juego_Ahorcado.py
@@ -90,29 +90,3 @@ while not juego_terminado:
 
     juego_terminado = terminado
 
-"""from random import choice
-from Funciones_ahorcado import pistas_palabras, elegir_palbra, pedir_letra, mostrar_nuevo_tablero, chequear_letra, perder, ganar
-palabras = ['panadero', 'dinosaurio', 'helipuerto', 'tiburon','cars','meteoro']
-letras_correctas = []
-letras_incorrectas = []
-intentos = 10
-aciertos = 0
-juego_terminado = False
-perder()
-ganar()
-
-palabra, letras_unicas = elegir_palbra(palabras)
-
-while not juego_terminado:
-    print('\n' + '*' * 20 + '\n')
-    pista_palabra(palabra)
-    mostrar_nuevo_tablero(palabra)
-    print('\n')
-    print('Letras incorrectas: ' + '-'.join(letras_incorrectas))
-    print(f'Vidas: {intentos}')
-    print('\n' + '*' * 20 + '\n')
-    letra = pedir_letra()
-
-    intentos, terminado, aciertos = chequear_letra(letra,palabra,intentos,aciertos)
-
-    juego_terminado = terminado"""
